62241747ed4084ecd90010246c5ac02deb3ab23c
ayyastakim <takimayyas@gmail.com>
2026-04-24 23:04:58 +0800

    initial commit

---
+++ number.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("satu = ", 1)
+	fmt.Println("dua = ", 2)
+	fmt.Println("tiga koma lima = ", 3.5)
+}
+
+// tipe data numerik di golang ada 2 yaitu integer dan float
+// integer adalah tipe data yang menyimpan bilangan bulat, sedangkan float adalah tipe data yang menyimpan bilangan desimal
+// tipe data integer di golang ada beberapa jenis yaitu int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64
+// tipe data float di golang ada 2 jenis yaitu float32 dan float64
+// tipe data numerik di golang juga bisa digunakan untuk melakukan operasi matematika seperti penjumlahan, pengurangan, perkalian, dan pembagian
