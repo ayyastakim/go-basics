3c5b3a681a14e8d7a202cfc0009408b1b4050d64
ayyastakim <takimayyas@gmail.com>
2026-05-27 22:33:31 +0800

    add example of go introduction

---
+++ 04-tipedatastring/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string type
+	fmt.Println("ayyas")
+	fmt.Println("ayyas takim")
+	fmt.Println("hello ayyas takim!")
+
+	// built-in function of string type
+	fmt.Println(len("ayyas"))
+	fmt.Println("ayyas takim"[0])
+	fmt.Println("hello ayyas takim!"[1])
+}
